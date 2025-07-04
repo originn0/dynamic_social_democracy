658de8e4ab4365b511e75763478338e785b8876c
GeneralGold <102259050+SylvurrTV@users.noreply.github.com>
2025-07-04 17:15:36 +0100

    added the sandbox tab, and a different background

--- out/html/game.js
+++ out/html/game.js
@@ -31,6 +31,14 @@
         window.dendryUI.dendryEngine.goToScene('library');
     }
   };
+
+  window.showSandbox = function() {
+      if (window.dendryUI.dendryEngine.state.sceneId.startsWith('sandbox')) {
+          window.dendryUI.dendryEngine.goToScene('backSpecialScene');
+      } else {
+          window.dendryUI.dendryEngine.goToScene('sandbox');
+      }
+  };
   
   window.showMods = function() {
     window.hideOptions();
